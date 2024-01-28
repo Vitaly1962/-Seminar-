060b5b1866d7ddeb907f1c64635d520d1c75c359
Vitaly <143294986+Vitaly1962@users.noreply.github.com>
2024-01-28 05:50:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # которая состоит всего из 1 столбца. Ваша задача перевести
 # его в one hot вид. Сможете ли вы это сделать без get_dummies?
 
-
 # import random
 # lst = ['robot'] * 10
 # lst += ['human'] * 10
@@ -20,28 +19,7 @@
 # # Выводим результат
 # result_df = pd.concat([data, one_hot_df], axis=1)
 # result_df.head()
-
-# ///////////////////Решение 1////////////////////////////////////
-# import random
-# import pandas as pd
-# from sklearn.preprocessing import LabelBinarizer
-
-# lst = ['robot'] * 10
-# lst += ['human'] * 10
-# random.shuffle(lst)
-# data = pd.DataFrame({'whoAmI': lst})
-
-# label_binarizer = LabelBinarizer()
-# one_hot_encoded = label_binarizer.fit_transform(data['whoAmI'])
-
-# # Создаем DataFrame с использованием бинарных представлений исходных классов
-# one_hot_df = pd.DataFrame(one_hot_encoded, columns=[label_binarizer.classes_[0] + "_encoded"])
-
-# # Объединяем исходный DataFrame с новым DataFrame
-# result_df = pd.concat([data, one_hot_df], axis=1)
-# print(result_df.head())
-
-# /////////////////////////Решение 2///////////////////////////
+# /////////////////////////Решение ///////////////////////////
 # метода map в pandas
 
 import random
@@ -57,3 +35,11 @@ data['human_encoded'] = data['whoAmI'].map({'robot': 1, 'human': 0})
 
 # Выводим результат
 print(data.head())
+
+#   whoAmI  human_encoded
+# 0  robot              1
+# 1  human              0
+# 2  robot              1
+# 3  robot              1
+# 4  robot              1
+
